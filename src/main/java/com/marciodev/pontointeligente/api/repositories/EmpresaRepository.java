66f8a367831839f0dd284ca369df0e800fa2e488
Marcio Cardoso <marcio.cardoso@totemti.com.br>
2022-06-18 17:27:55 -0400

    Adicionando pacote de repositories

---
+++ src/main/java/com/marciodev/pontointeligente/api/repositories/EmpresaRepository.java
@@ -0,0 +1,13 @@
+package com.marciodev.pontointeligente.api.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.marciodev.pontointeligente.api.entidades.Empresa;
+
+public interface EmpresaRepository extends JpaRepository<Empresa, Long>{
+	
+	@Transactional(readOnly = true)
+	Empresa findByCnpj(String cnpj);
+
+}
